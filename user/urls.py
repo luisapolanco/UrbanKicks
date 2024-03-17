4e32e51385966f19a2bb99865801561c2b668194
Mig <maescuderc@eafit.edu.co>
2024-03-17 12:17:33 -0500

    Login working for adm user

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CustomerSignUpView, AdmSignUpView
+from .views import CustomerSignUpView, AdmSignUpView, LoginView
 
 urlpatterns = [
     path('user/customer_sign_up', CustomerSignUpView.as_view(), name='customer_sign_up'),
     path('user/adm_sign_up', AdmSignUpView.as_view(), name='adm_sign_up'),
+    path('user/login', LoginView.as_view(), name='login'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,27 +1,61 @@
 from django.shortcuts import render, redirect
-from .forms import CustomerSignUpFrom, AdmSignUpFrom
+from .forms import CustomerSignUpFrom, AdmSignUpFrom, LoginForm
 from django.views import View
+from django.contrib.auth import login, authenticate
+from django.contrib.auth import views as auth_views
+from django.urls import reverse
 # Create your views here.
 
 class CustomerSignUpView(View):
     def get(self, request):
         form = CustomerSignUpFrom
-        return render(request, 'customer_sign_up.html', context={'form':form})
+        return render(request, 'registration/customer_sign_up.html', context={'form':form})
     def post(self, request):
         form = CustomerSignUpFrom(request.POST)
         if form.is_valid():
             form.save()
             return redirect('home')
-        return render(request, 'customer_sign_up.html', context={'form':form})
+        return render(request, 'registration/customer_sign_up.html', context={'form':form})
     
 class AdmSignUpView(View):
     def get(self, request):
         form = AdmSignUpFrom
-        return render(request, 'adm_sign_up.html', context={'form':form})
+        return render(request, 'registration/adm_sign_up.html', context={'form':form})
     def post(self, request):
         form = AdmSignUpFrom(request.POST)
         if form.is_valid():
             form.save()
             return redirect('home')
-        return render(request, 'adm_sign_up.html', context={'form':form})
-    
+        return render(request, 'registration/adm_sign_up.html', context={'form':form})
+
+class LoginView(auth_views.LoginView):
+    form_class = LoginForm
+    template_name = "registration/login.html"
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(**kwargs)
+
+    def get_success_url(self):
+        user = self.request.user
+        if user.is_authenticated:
+            return reverse('home')
+
+        else: 
+            return reverse('login')
+
+'''class LoginView(View):
+    form = LoginForm
+    def get(self, request):
+        form = LoginForm
+        return render(request, 'registration/login.html', context={'form':form})
+    def post(self, request):
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password'],
+            )
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+        return render(request, 'registration/login.html', context={'form':form})'''
