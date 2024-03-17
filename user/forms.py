58393a7c634ec5a0c2d25c26f5434470f7c33f6e
Mig <maescuderc@eafit.edu.co>
2024-03-16 22:05:02 -0500

    User Table (complete) and sign up forms

--- user/forms.py
+++ user/forms.py
@@ -1,20 +1,27 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm 
 from django.db import transaction
 from django import forms
-from .models import User, Cliente, Adm
+from .models import User, Customer, Adm
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
 class ClienteSignUpFrom(UserCreationForm):
     email = forms.EmailField(widget=forms.EmailInput())
-    Nombre = forms.CharField(widget=forms.TextInput())
+    name = forms.CharField(widget=forms.TextInput())
+    city = forms.CharField(max_lenght = 100, widget=forms.TextInput())
+    address = forms.CharField(max_lenght = 100, widget=forms.TextInput())
+    phone_number = forms.CharField(max_lenght=10, widget=forms.TextInput())
+
+    payment_info = forms.CharField(max_lenght = 100, widget=forms.TextInput())
+    customer_id = forms.IntegerField(max_lenght= 10, widget=forms.NumberInput())
+
     password1 = forms.CharField(widget=forms.PasswordInput())
     password2 = forms.CharField(widget=forms.PasswordInput())
 
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ('username', 'email', 'name', 'city', 'address', 'phone_number', 'password1', 'password2')
     
     @transaction.atomic
     def save(self, commit=True):
@@ -22,11 +29,34 @@ class ClienteSignUpFrom(UserCreationForm):
         user.is_cliente = True
         if commit:
             user.save()
-        cliente = Cliente.objects.create(user=user, Nombre = self.cleaned_data.get('Nombre'))
+        cliente = Customer.objects.create(user=user, payment_info = self.cleaned_data.get('payment_info'), customer_id = self.cleaned_data.get('customer_id'))
         return user
 
 class AdmSignUpFrom(UserCreationForm):
     email = forms.EmailField(widget=forms.EmailInput())
+    name = forms.CharField(widget=forms.TextInput())
+    city = forms.CharField(max_lenght = 100, widget=forms.TextInput())
+    address = forms.CharField(max_lenght = 100, widget=forms.TextInput())
+    phone_number = forms.CharField(max_lenght=10, widget=forms.TextInput())
+
+    adm_id = forms.IntegerField(max_lenght= 10, widget=forms.NumberInput())
+
+    password1 = forms.CharField(widget=forms.PasswordInput())
+    password2 = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ('username', 'email', 'name', 'city', 'address', 'phone_number', 'password1', 'password2')
+    
+    @transaction.atomic
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.is_adm = True
+        if commit:
+            user.save()
+        adm = Adm.objects.create(user=user, adm_id = self.cleaned_data.get('adm_id'))
+        return user
+
 class LoginForm(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput())
     password = forms.CharField(widget=forms.PasswordInput())    
--- user/models.py
+++ user/models.py
@@ -4,6 +4,10 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     email = models.EmailField(max_length=254, unique=True)
+    name = models.CharField(max_lenght = 100)
+    city = models.CharField(max_lenght = 100)
+    address = models.CharField(max_lenght = 100)
+    phone_number = models.CharField(max_lenght=10)
     is_cliente = models.BooleanField(default=False) 
     is_adm = models.BooleanField(default=False) # Persona encargada de agregar productos
 
@@ -15,16 +19,17 @@ class User(AbstractUser):
     def is_Adm(self):
         return self.is_adm
 
-class Cliente(models.Model):
+class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, related_name='Cliente')
-    Nombre = models.CharField(max_length=100)
+    payment_info = models.CharField(max_length=100)
+    customer_id = models.IntegerField(max_length = 10)
 
     def __str__(self):
-        return self.Nombre
+        return self.customer_id
 
 class Adm(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, related_name='Adm', default='')
-    Nombre_Empresa = models.CharField(max_length=100, unique=True)
+    adm_id = models.IntegerField(max_length = 10)
 
     def __str__(self):
-        return self.Nombre_Empresa
+        return self.adm_id
